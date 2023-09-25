14d3fe2342e455a53a7a810a9fc8bfafa7dc1104
Kartikayrai13 <kartikayrai09@gmail.com>
2023-09-25 21:48:42 +0530

    Initial Commit

--- Interviews/src/main/java/JDBC/Select.java
+++ Interviews/src/main/java/JDBC/Select.java
@@ -12,9 +12,9 @@ public static void main(String[] args) throws Exception {
 	Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/practise","root","root");
 
 	PreparedStatement ps = conn.prepareStatement("Select first_name,age from student");
-	//ResultSet rs = ps.executeQuery();
-	Statement s = conn.createStatement();
-	ResultSet rs = s.executeQuery("Select first_name,age from student");
+	ResultSet rs = ps.executeQuery();
+	//Statement s = conn.createStatement();
+	//ResultSet rs = s.executeQuery("Select first_name,age from student");
 	
 		
 	while(rs.next()) {
